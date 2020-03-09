748321484640bd5709088a217e4a5b6444bda7a0
root <root@localhost.localdomain>
2020-03-09 08:02:57 -0400

    delete temp files

--- driver/arm-driver/yuanzige/1_chrdevbase/.chrdevbase.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.ko /home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.o /home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.mod.o
--- driver/arm-driver/yuanzige/1_chrdevbase/.tmp_versions/chrdevbase.mod
+++
@@ -1,2 +0,0 @@
-/home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.ko
-/home/duo/work/git/mp3/driver/arm-driver/yuanzige/1_chrdevbase/chrdevbase.o
